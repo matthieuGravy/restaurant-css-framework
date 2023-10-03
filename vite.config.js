4c2d65625fe59effc087df99079f29c9952f111b
matthieuGravy <matthieugravy@gmail.com>
2023-10-03 14:36:30 +0200

    page home

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import path from "path";
+
+export default {
+  root: path.resolve(__dirname, "src"),
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+  build: {
+    outDir: "../dist",
+  },
+  server: {
+    port: 8080,
+  },
+};
