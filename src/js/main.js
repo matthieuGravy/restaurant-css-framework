02ee553a9739cb60dbae7b55c725030ac8af9af2
matthieuGravy <matthieugravy@gmail.com>
2023-10-04 16:40:18 +0200

    return

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,18 @@ import "../scss/style.scss";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+
+//pagination
+/* 
+possibilité #1
+``````````````
+objectif : 
+1. images dans un tableau 
+2. itérer au clic 
+3. selectionner
+4. rendre le tabmeai 
+
+° 4 tableaux de 3 images
+° ajouter des descriptions
+
+*/
